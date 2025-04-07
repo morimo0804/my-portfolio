798a48a75e13a4f09c6a79f431e6b9195283b09d
morimo0804 <hayato.morimo.0804@gmail.com>
2025-04-07 23:53:13 +0900

    途中

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -1,26 +1,29 @@
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./index.module.css";
+import { s } from "framer-motion/client";
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h1 className={styles.logo}>
-        <Link href="/">HAYATO PORTFOLIO</Link>
-      </h1>
-      <nav className={styles.nav}>
-        <ul className={styles.items}>
-          <li>
-            <Link href="/about">ABOUT</Link>
-          </li>
-          <li>
-            <Link href="/works">WORKS</Link>
-          </li>
-          <li>
-            <Link href="/#area1">CONTACT</Link>
-          </li>
-        </ul>
-      </nav>
+      <div className={styles.inner}>
+        <h1 className={styles.logo}>
+          <Link href="/">HAYATO PORTFOLIO</Link>
+        </h1>
+        <nav className={styles.nav}>
+          <ul className={styles.items}>
+            <li>
+              <Link href="/about">ABOUT</Link>
+            </li>
+            <li>
+              <Link href="/works">WORKS</Link>
+            </li>
+            <li>
+              <Link href="/#area1">CONTACT</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
